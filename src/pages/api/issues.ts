3a0a7499ee1b960e7873545f7dd5dbdb59ab31b3
kthatoto <kthatoto@gmail.com>
2023-03-21 04:26:37 +0900

    Add create issue mutation

--- src/pages/api/issues.ts
+++ src/pages/api/issues.ts
@@ -2,6 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { graphqlClient } from "@/utils/graphqlClient";
 
+const OWNER = "kthatoto";
+const REPOSITORY = "message-boards";
+
 export type Issue = {
   number: number;
   title: string;
@@ -20,29 +23,81 @@ type IssuesResponse = {
   };
 };
 
-const query = `
-  query ($first: Int!) {
-    repository(owner: "kthatoto", name: "message-boards") {
-      issues(
-        first: $first
-        orderBy: {field: CREATED_AT, direction: DESC}
-        states: OPEN
-      ) {
-        nodes {
-          number
-          title
-          bodyText
-          createdAt
-          comments {
-            totalCount
-          }
+type CreateIssueResponse = {
+  createIssue: {
+    issue: Issue;
+  };
+};
+
+const getRepositoryIssues = `
+query ($owner: String!, $repository: String!, $first: Int!) {
+  repository(owner: $owner, name: $repository) {
+    id
+    issues(
+      first: $first
+      orderBy: {field: CREATED_AT, direction: DESC}
+      states: OPEN
+    ) {
+      nodes {
+        number
+        title
+        bodyText
+        createdAt
+        comments {
+          totalCount
         }
       }
     }
   }
+}
 `;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const issuesResponse = await graphqlClient<IssuesResponse>(query, { first: 10 });
+const createIssue = `
+mutation ($repositoryId: ID!, $title: String!, $body: String) {
+  createIssue(input: {
+    repositoryId: $repositoryId,
+    title: $title,
+    body: $body,
+  }) {
+    issue {
+      number
+      title
+      bodyText
+      createdAt
+      comments {
+        totalCount
+      }
+    }
+  }
+}
+`;
+
+const get = async (req: NextApiRequest, res: NextApiResponse) => {
+  const issuesResponse = await graphqlClient<IssuesResponse>(
+    getRepositoryIssues,
+    { owner: OWNER, repository: REPOSITORY, first: 10 }
+  );
   return res.status(200).json(issuesResponse.repository.issues.nodes);
+};
+
+const post = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { repositoryId, title, body } = req.body;
+  if (!repositoryId || !title) {
+    return res.status(400);
+  }
+  const createIssueResponse = await graphqlClient<CreateIssueResponse>(
+    createIssue,
+    { repositoryId, title, body }
+  );
+  return res.status(201).json(createIssueResponse.createIssue.issue);
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    await get(req, res);
+  } else if (req.method === "POST") {
+    await post(req, res);
+  } else {
+    throw new Error(`unsupported method: ${req.method}`)
+  }
 };
