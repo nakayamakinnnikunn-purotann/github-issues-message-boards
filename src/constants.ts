211d0765e2d85e5c8ec46eb4a66ddd05b3b1fa14
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2023-07-26 20:01:52 +0900

    Delete src directory

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const OWNER = "kthatoto";
-export const REPOSITORY = "message-boards";
--- src/utils/graphqlClient.ts
+++
@@ -1,7 +0,0 @@
-import { graphql } from "@octokit/graphql";
-
-export const graphqlClient = graphql.defaults({
-  headers: {
-    authorization: `Token ${process.env.GITHUB_TOKEN}`,
-  },
-});
